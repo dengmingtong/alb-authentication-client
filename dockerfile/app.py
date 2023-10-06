6c52eb3a5aecdbff708f09b0c709f4741a67f001
dengmingtong <dengmingtong@hotmail.com>
2023-10-07 00:18:11 +0800

    refactor code

--- dockerfile/app.py
+++ dockerfile/app.py
@@ -1,28 +1,53 @@
 import logging
 import os
 import json
-import base64
 import time
+import boto3
+import requests
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-# example to get cookie:
-ALB_URL = ''
-USERNAME = ''
-PASSWORD = ''
+secrets_manager_client = boto3.client('secretsmanager') 
+server_url = os.environ.get('SERVER_URL')
+user_secret_manager_arn = os.environ.get('USER_SECRET_MANAGER_ARN')
+cookie_secret_manager_name = os.environ.get('COOKIE_SECRET_MANAGER_NAME')
 
 log = logging.getLogger()
 log.setLevel('INFO')
 
 def handler(event, context):
-    cookie = get_cookies(ALB_URL, USERNAME, PASSWORD)
-    # 你的代码逻辑
+    api_url = f"{server_url}/collect"
+    cookie = get_valid_cookie_from_secret_manager()
+    if cookie != None:
+        log.info("cookie is existing, and not expire")
+        call_authentication_api(api_url, cookie)
+        return {
+            "statusCode": 200,
+            "body": json.dumps("successful!")
+        }
+    
+    secret_values = get_secret_values(user_secret_manager_arn)
+    
+    # 使用 secret_values 中的键值对
+    user_name = secret_values['username']
+    password = secret_values['password']
+    login_url = f"{server_url}/login"
+
+    log.info(f"login_url: {login_url}")
+    log.info(f"user_name: {user_name}")
+    log.info(f"password: {password}")
+
+    cookie = get_cookies(login_url, user_name, password)
+    store_cookie(cookie)
+    
+    call_authentication_api(api_url, cookie)
+
     return {
         "statusCode": 200,
-        "body": json.dumps(cookie)
+        "body": json.dumps("successful!")
     }
 
 def get_cookies(alb_url, username, password):
@@ -45,15 +70,6 @@ def get_cookies(alb_url, username, password):
     driver.get(alb_url)
     
     WebDriverWait(driver, 20).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-    # simulate to input username, password and submit
-    # e_index = 0
-    # username_input = driver.find_elements(by=By.NAME, value="username")[e_index]
-    # username_input.send_keys(username)    
-    # password_input = driver.find_elements(by=By.NAME, value="password")[e_index]
-    # password_input.send_keys(password)
-    # submit_button = driver.find_elements(by=By.NAME, value="action")[e_index]
-    # submit_button.click() 
-
     e_index = 0
     username_input = driver.find_elements(by=By.NAME, value="username")[e_index]
     username_input.send_keys(username)
@@ -85,6 +101,62 @@ def get_cookies(alb_url, username, password):
         cookies_values.append(f"{c_name}={c_value}")
     cookies_header = "; ".join(cookies_values)
     return cookies_header
+
+def get_secret_values(secret_arn):
+    response = secrets_manager_client.get_secret_value(SecretId=secret_arn)
+    secret_string = response['SecretString']
+    secret_values = json.loads(secret_string)
+    
+    return secret_values
+
+def store_cookie(cookie):
+    createTime = int(time.time())
+    try:
+        secrets_manager_client.describe_secret(SecretId=cookie_secret_manager_name)
+    except secrets_manager_client.exceptions.ResourceNotFoundException:
+        secrets_manager_client.create_secret(
+          Name=cookie_secret_manager_name,
+          Description='This secret stores the ALB cookie',
+          SecretString=json.dumps({
+              "Cookie": cookie,
+              "expireAt": createTime + 14400   # 4 hours
+            })
+        )
+    else:
+        secrets_manager_client.put_secret_value(
+            SecretId=cookie_secret_manager_name,
+            SecretString=json.dumps({
+                "Cookie": cookie,
+                "expireAt": createTime + 14400   # 4 hours
+              })
+        )
+
+def get_valid_cookie_from_secret_manager():
+    current_time = int(time.time())
+    try:
+        secrets_manager_client.describe_secret(SecretId=cookie_secret_manager_name)
+    except secrets_manager_client.exceptions.ResourceNotFoundException:
+        return None
+    else:
+        response = secrets_manager_client.get_secret_value(SecretId=cookie_secret_manager_name)
+        secret_string = response['SecretString']
+        secret_values = json.loads(secret_string)
+        expire_at = int(secret_values['expireAt'])
+        if (current_time > expire_at + 600):
+            return None
+        cookie = secret_values['Cookie']
+
+        return cookie
+    
+def call_authentication_api(api_url, cookie):
+  data = f"this is a test"
+  headers = {
+      "Cookie": cookie
+  }
+  response = requests.post(api_url, headers=headers, data=data)
+  log.info(f"response code: {response.status_code}")
+
+
     
      
 
--- lib/alb-authentication-client-stack.ts
+++ lib/alb-authentication-client-stack.ts
@@ -2,23 +2,78 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {DockerImageFunction, DockerImageCode} from 'aws-cdk-lib/aws-lambda';
 import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import {
+  StackProps,
+  CfnParameter,
+} from 'aws-cdk-lib';
+import {
+  Role,
+  PolicyStatement,
+} from 'aws-cdk-lib/aws-iam';
 
 export class AlbAuthenticationClientStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const serverUrlParam = new CfnParameter(
+      this, 
+      "ServerUrl", 
+      {
+        description: 'ALB Url',
+        type: 'String',
+      },
+    );
+    
+    const userInfoSecretManagerArnParam = new CfnParameter(
+      this, 
+      "UserInfoSecretManagerArn", 
+      {
+        description: 'Secret Manager Arn for user name and password',
+        type: 'String',
+      },
+    );
+
+    const cookieSecretManagerNameParam = new CfnParameter(
+      this, 
+      "CookieSecretManagerName", 
+      {
+        description: 'Secret Manager Arn for user name and password',
+        type: 'String',
+      },
+    );     
 
     // 从Dockerfile创建一个Lambda函数
     const lambdaFunction = new DockerImageFunction(this, 'MyFunction', {
       code: DockerImageCode.fromImageAsset('./dockerfile'),
       timeout: cdk.Duration.seconds(60),
+      memorySize: 1024,
+      environment: {
+        "SERVER_URL": serverUrlParam.valueAsString,
+        "USER_SECRET_MANAGER_ARN": userInfoSecretManagerArnParam.valueAsString,
+        "COOKIE_SECRET_MANAGER_NAME": cookieSecretManagerNameParam.valueAsString,
+      }
     });
+    const lambdaRole = lambdaFunction.role as Role;
+    lambdaRole.addToPolicy(
+      new PolicyStatement({
+        actions: [
+          'secretsmanager:GetSecretValue',
+          'secretsmanager:CreateSecret',
+          'secretsmanager:PutSecretValue',
+          'secretsmanager:DescribeSecret',
+        ],
+        resources: ['*'],
+      })
+    );
 
-    // 创建API Gateway
     const api = new RestApi(this, 'TestAPI');
 
-    const lambdaIntegration = new LambdaIntegration(lambdaFunction);
+    const lambdaIntegration = new LambdaIntegration(
+      lambdaFunction,
+      {
+        timeout: cdk.Duration.seconds(29),
+      }
+    );
 
-    // 创建资源和方法
     const resource = api.root.addResource('myresource');
     resource.addMethod('GET', lambdaIntegration);
   }
